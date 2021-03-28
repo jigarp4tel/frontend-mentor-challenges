d1daf8bf70caef6fa1755bcd311d78b7a8d90a8a
Jigar Patel <44246251+jigarp4tel@users.noreply.github.com>
2021-03-28 03:27:35 -0500

    -updated style

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,13 +1,18 @@
 const themeToggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefer-color-scheme: dark)").matches ? "dark" : "light");
+
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme);
+
+
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        console.log("Dark-theme");
     } else {
-        document.documentElement.setAttribute('data-theme','light');
-        console.log("Light-theme");
+        document.documentElement.setAttribute('data-theme', 'light');
     }
 }
 
-themeToggleSwitch.addEventListener('change',switchTheme, false);
+themeToggleSwitch.addEventListener('change', switchTheme, false);
+console.log(storedTheme);
